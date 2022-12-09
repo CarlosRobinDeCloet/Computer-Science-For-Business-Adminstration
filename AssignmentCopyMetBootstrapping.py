5c9cfb5e5d88e6e1d9e8d4061c53411113a5af43
Lenovo-PC\Carlos de Cloet <carlosdecloet@gmail.com>
2022-12-09 15:15:11 +0100

    Final version

--- AssignmentCopyMetBootstrapping.py
+++ AssignmentCopyMetBootstrapping.py
@@ -172,225 +172,201 @@ F1BS  = []
 PR = []
 RC = []
 
-
-bands = [5,13,26,50,65,130]
+results = []
+bands = [65] #[5, 10, 13, 25, 26, 50, 65, 130, 325]
     
-#####################################################################################################################
-# Bootstrapped data.
-#####################################################################################################################
-
-for i in range(5):                                                                
-    
-    print("Iteration " + str(i+1) + " of bootstrapping the data. Elapsed time is: " + str(time.time()-starttime))
-
-    bootstrappedData = bootstrapping(tvs)
+    #####################################################################################################################
+    # Bootstrapped data.
+    #####################################################################################################################
+for band in range(len(bands)):
+    results.append([])
+    for i in range(5):                                                                
         
-    trainingData = bootstrappedData[0]
-    testData = bootstrappedData[1]
-    n = len(trainingData)
+        print("Iteration " + str(i+1) + " of bootstrapping the data. Elapsed time is: " + str(time.time()-starttime))
+
+        bootstrappedData = bootstrapping(tvs)
         
-    titlesTrain = []
-    for dict in trainingData:
-        titlesTrain.append(dict['title'])
-        dict['modelwords'] = []
+        trainingData = bootstrappedData[0]
+        testData = bootstrappedData[1]
+        n = len(trainingData)
+        
+        titlesTrain = []
+        for dict in trainingData:
+            titlesTrain.append(dict['title'])
+            dict['modelwords'] = []
        
-    titlesTest = []
-    for dict in testData:
-        titlesTest.append(dict['title'])
-        dict['modelwords'] = []
+        titlesTest = []
+        for dict in testData:
+            titlesTest.append(dict['title'])
+            dict['modelwords'] = []
     
-    wordsWithStringAndChar = set()
-    countWordsWithStringAndChar = []
-    for i in range(len(titlesTrain)):
-        modelwords = re.finditer('[a-zA-Z0-9]*(([0-9]+[^0-9, ]+)|([^0-9, ]+[0-9]+))[a-zA-Z0-9]*' ,titlesTrain[i])
-        for candidateWord in modelwords:
-            trainingData[i]['modelwords'].append(candidateWord.group())
-            wordsWithStringAndChar.add(candidateWord.group())
-            countWordsWithStringAndChar.append(candidateWord.group())
-            
-    for products in trainingData:
-        for key in products['featuresMap'].keys():
-            modelwords = re.finditer('(\d+\:+\d)',products['featuresMap'][key])
+        wordsWithStringAndChar = set()
+        countWordsWithStringAndChar = []
+        for i in range(len(titlesTrain)):
+            modelwords = re.finditer('[a-zA-Z0-9]*(([0-9]+[^0-9, ]+)|([^0-9, ]+[0-9]+))[a-zA-Z0-9]*' ,titlesTrain[i])
             for candidateWord in modelwords:
                 trainingData[i]['modelwords'].append(candidateWord.group())
                 wordsWithStringAndChar.add(candidateWord.group())
                 countWordsWithStringAndChar.append(candidateWord.group())
-
-    listOfWords = list(wordsWithStringAndChar)
-    listOfweights =[]
-    wordCounter = {}
-    for word in listOfWords:
-        wordCounter[word] = []
-        
-    for word in countWordsWithStringAndChar:
-        wordCounter[word].append(1)
-        
-    for word in listOfWords:
-        wordCounter[word] = 1/sum(wordCounter[word])
-        if wordCounter[word] == 1:
-            wordCounter[word] = 0.0
-      
-    listOfWeights = []
-    for word in listOfWords:
-        listOfWeights.append(wordCounter[word])
-    
-
-    oneHotEncoded = []
-
-    for i in range(len(titlesTrain)):
-        oneHotEncoded.append([1 if x in titlesTrain[i].split() else 0 for x in listOfWords])
-    
-    
-    
-    prime = 4549
-
-    signatureMatrix = np.empty([650,len(oneHotEncoded)]) ### [oneHotEncoded/2, oneHotEncoded]
-    signatureMatrix.fill(99999)
-
-
-    a = np.random.randint(0,1000,size = len(signatureMatrix))
-    b = np.random.randint(1,1000,size = len(signatureMatrix))
-
-    for r in range(len(oneHotEncoded[0])):
-     
-        hashValues = hashFunction(a,b,prime,r)
-        
-        #if r%50 == 0:
-        #    print('Current iteration for minhashing is: ' + str(r) + " Elapsed time is: " + str(time.time()-starttime))
-    
-    
-        for i in range(len(oneHotEncoded)):
-            if oneHotEncoded[i][r] == 1:
-                for j in range(len(signatureMatrix)):
-                    if hashValues[j] < signatureMatrix[j][i]:
-                        signatureMatrix[j][i] = hashValues[j]
             
-    for i in range(len(signatureMatrix)):
-        for j in range(len(signatureMatrix[i])):
-            if signatureMatrix[i][j] == 99999:
-                print('ohno')
+        for products in trainingData:
+            for key in products['featuresMap'].keys():
+                modelwords = re.finditer('(\d+\:+\d)',products['featuresMap'][key])
+                for candidateWord in modelwords:
+                    trainingData[i]['modelwords'].append(candidateWord.group())
+                    wordsWithStringAndChar.add(candidateWord.group())
+                    countWordsWithStringAndChar.append(candidateWord.group())
+
+        listOfWords = list(wordsWithStringAndChar)
+   
+        oneHotEncoded = []
+
+        for i in range(len(titlesTrain)):
+            oneHotEncoded.append([1 if x in titlesTrain[i].split() else 0 for x in listOfWords])
+    
+        signatureMatrix = np.empty([650,len(oneHotEncoded)]) # 650 equals 50% of the model words
+        signatureMatrix.fill(99999)
+
+        a = np.random.randint(0,1000,size = len(signatureMatrix))
+        b = np.random.randint(1,1000,size = len(signatureMatrix))
+        prime = 4549
+        
+        for r in range(len(oneHotEncoded[0])):
+     
+            hashValues = hashFunction(a,b,prime,r)
+        
+            #if r%50 == 0:
+            #    print('Current iteration for minhashing is: ' + str(r) + " Elapsed time is: " + str(time.time()-starttime))
+    
+    
+            for i in range(len(oneHotEncoded)):
+                if oneHotEncoded[i][r] == 1:
+                    for j in range(len(signatureMatrix)):
+                        if hashValues[j] < signatureMatrix[j][i]:
+                            signatureMatrix[j][i] = hashValues[j]
+            
+        for i in range(len(signatureMatrix)):
+            for j in range(len(signatureMatrix[i])):
+                if signatureMatrix[i][j] == 99999:
+                    print('ohno')
     
                                                                       
-    ## 650 is divisible by {1, 2, 5, 10, 13, 15, 25, 26, 50, 65, 130, 325, and 650}
+        ## 650 is divisible by {1, 2, 5, 10, 13, 15, 25, 26, 50, 65, 130, 325, and 650}
 
-    subVectorMatrix = split_signatureMatrix(signatureMatrix, 65)
-    print("Finished splitting vectors. Elapsed time is: " + str(time.time() - starttime))
+        subVectorMatrix = split_signatureMatrix(signatureMatrix, bands[band])
+        print("Finished splitting vectors. Elapsed time is: " + str(time.time() - starttime))
 
-
-    buckets = []
-    for i in range(10000000):
-        buckets.append([])  
+        buckets = []
+        for i in range(10000000):
+            buckets.append([])  
     
+        binVectors(subVectorMatrix, buckets)
+        print("Finished bucketing vectors. Elapsed time is: " + str(time.time() - starttime))
 
-        
-    binVectors(subVectorMatrix, buckets)
-    print("Finished bucketing vectors. Elapsed time is: " + str(time.time() - starttime))
-
-
-    listOfCandidatePairs = []
-    numCol = 0
-    for i in range(len(buckets)):
-        if len(buckets[i]) > 1:
-            listOfCandidatePairs.append([])
-            for j in range(len(buckets[i])):
-                listOfCandidatePairs[numCol].append(buckets[i][j])
-            numCol += 1
+        listOfCandidatePairs = []
+        numCol = 0
+        for i in range(len(buckets)):
+            if len(buckets[i]) > 1:
+                listOfCandidatePairs.append([])
+                for j in range(len(buckets[i])):
+                    listOfCandidatePairs[numCol].append(buckets[i][j])
+                numCol += 1
                 
-    print("Finished gathering candidate pairs. Elapsed time is: " + str(time.time() - starttime))  
+        print("Finished gathering candidate pairs. Elapsed time is: " + str(time.time() - starttime))  
 
-    candidatePairsMatrix = np.empty([n,n])
-    candidatePairsMatrix.fill(False)
+        candidatePairsMatrix = np.empty([n,n])
+        candidatePairsMatrix.fill(False)
 
-    for i in listOfCandidatePairs:
-        for j in i:
-            for k in i:
-                candidatePairsMatrix[j][k] = True
-                if trainingData[j]['shop'] == trainingData[k]['shop']:     ######### These 2 lines were added
-                    candidatePairsMatrix[j][k] = False
-                if trainingData[j]['brand'] != [] and trainingData[k]['brand'] != [] and trainingData[j]['brand'] != trainingData[k]['brand']:
-                    candidatePairsMatrix[j][k] = False
-
-                  
-    duplicateFound = 0
-    duplicateTotal = 0
-    amountOfComparisonsMade = 0
-    for i in range(len(candidatePairsMatrix)):
-        for j in range(len(candidatePairsMatrix[i])):
-            if i < j:
-                if trainingData[i]['modelID'] == trainingData[j]['modelID']:
-                    duplicateTotal += 1
-                if candidatePairsMatrix[i][j] == 1:
-                    amountOfComparisonsMade += 1
+        for i in listOfCandidatePairs:
+            for j in i:
+                for k in i:
+                    candidatePairsMatrix[j][k] = True
+                    if trainingData[j]['shop'] == trainingData[k]['shop']:
+                        candidatePairsMatrix[j][k] = False
+                    if trainingData[j]['brand'] != [] and trainingData[k]['brand'] != [] and trainingData[j]['brand'] != trainingData[k]['brand']:
+                        candidatePairsMatrix[j][k] = False
+         
+        duplicateFound = 0
+        duplicateTotal = 0
+        amountOfComparisonsMade = 0
+        for i in range(len(candidatePairsMatrix)):
+            for j in range(len(candidatePairsMatrix[i])):
+                if i < j:
                     if trainingData[i]['modelID'] == trainingData[j]['modelID']:
-                        duplicateFound += 1
-                
-            
-    pairQuality = duplicateFound/amountOfComparisonsMade
+                        duplicateTotal += 1
+                    if candidatePairsMatrix[i][j] == 1:
+                        amountOfComparisonsMade += 1
+                        if trainingData[i]['modelID'] == trainingData[j]['modelID']:
+                            duplicateFound += 1
+                           
+        pairQuality = duplicateFound/amountOfComparisonsMade
+        pairCompleteness = duplicateFound / duplicateTotal
+        F1 = (2*pairQuality*pairCompleteness)/(pairQuality+pairCompleteness)
 
-    pairCompleteness = duplicateFound / duplicateTotal
-
-    F1 = (2*pairQuality*pairCompleteness)/(pairQuality+pairCompleteness)
-
-    #USING JACCARD SIMILARITY AS FIRST EXPLORATION!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    dissimilarityMatrix = np.empty([n,n])
-    dissimilarityMatrix.fill(99999)
+        #USING COSINE SIMILARITY
+        dissimilarityMatrix = np.empty([n,n])
+        dissimilarityMatrix.fill(99999)
     
 
-    for i in range(len(candidatePairsMatrix)):
-        for j in range(len(candidatePairsMatrix[i])):
-            if i < j:
-                if candidatePairsMatrix[i][j] == 1:
-                    #modelWordsI = set(trainingData[i]['modelwords'])
-                    #modelWordsJ = set(trainingData[j]['modelwords'])
-                    #dissimilarityMatrix[i,j] = 1 - jaccard_similarity(modelWordsI, modelWordsJ)
+        for i in range(len(candidatePairsMatrix)):
+            for j in range(len(candidatePairsMatrix[i])):
+                if i < j:
+                    if candidatePairsMatrix[i][j] == 1:
+                        #modelWordsI = set(trainingData[i]['modelwords'])
+                        #modelWordsJ = set(trainingData[j]['modelwords'])
+                        #dissimilarityMatrix[i,j] = 1 - jaccard_similarity(modelWordsI, modelWordsJ)
                                            
+                        binaryVectorI = oneHotEncoded[i]
+                        binaryVectorJ = oneHotEncoded[j]
+                        dissimilarityMatrix[i,j] = 1 - cosine_similarity(binaryVectorI,binaryVectorJ)
                     
-                    binaryVectorI = oneHotEncoded[i]
-                    binaryVectorJ = oneHotEncoded[j]
-                    dissimilarityMatrix[i,j] = 1 - cosine_similarity(binaryVectorI,binaryVectorJ)
-                    
-    predictedDuplicateMatrix = np.empty([n,n])
-    predictedDuplicateMatrix.fill(False)
+        predictedDuplicateMatrix = np.empty([n,n])
+        predictedDuplicateMatrix.fill(False)
 
-    # Threshold should be between 0 and 1    
-    classifyAsDuplicates(predictedDuplicateMatrix, dissimilarityMatrix, 0.8)    
+        # Threshold should be between 0 and 1    
+        classifyAsDuplicates(predictedDuplicateMatrix, dissimilarityMatrix, 0.2)    
     
-    print('Calculating similarities finished. Elapsed time is: ' + str(time.time() - starttime))       
+        print('Calculating similarities finished. Elapsed time is: ' + str(time.time() - starttime))       
  
-    f1 = calculatingPrecisionAndRecall(predictedDuplicateMatrix, trainingData)
+        f1 = calculatingPrecisionAndRecall(predictedDuplicateMatrix, trainingData)
 
-    endtime = time.time()
+        endtime = time.time()
     
-    PCBS.append(pairCompleteness)
-    PQBS.append(pairQuality)
-    F1BS.append(f1[0])
-    F1asteriskBS.append(F1)
-    PR.append(f1[1])
-    RC.append(f1[2])
+        PCBS.append(pairCompleteness)
+        PQBS.append(pairQuality)
+        F1BS.append(f1[0])
+        F1asteriskBS.append(F1)
+        PR.append(f1[1])
+        RC.append(f1[2])
 
+        print("")
+        print("The pair quality is: " + str(pairQuality*100) + "%.")
+        print("The pair completeness is: " + str(pairCompleteness*100) + "%.")
+        print("The F1* score is: " + str(F1))
+        print("The F1 score is: " + str(f1[0]))
+        print("")
+    
+    print("Program finished. Total elapsed time is: " + str(endtime - starttime))
+
+    avgPQ = np.average(PQBS)
+    avgPC = np.average(PCBS)
+    avgF1asterisks = np.average(F1asteriskBS)
+    avgF1 = np.average(F1BS)
+    avgPR = np.average(PR)
+    avgRC = np.average(RC)
+        
     print("")
-    print("The amount of bands is: " + str(50) + " and the amount of rows is: " + str(650/50))
-    print("The pair quality is: " + str(pairQuality*100) + "%.")
-    print("The pair completeness is: " + str(pairCompleteness*100) + "%.")
-    print("The F1* score is: " + str(F1*100))
-    print("The F1 score is: " + str(f1*100))
+    print("The average pair quality is: " + str(avgPQ*100) + "%.")
+    print("The average pair completeness is: " + str(avgPC*100) + "%.")
+    print("The average F1* score is: " + str(avgF1asterisks))
+    print("The average F1 score is: " + str(avgF1))
+    print("The average precision score is: " + str(avgPR))
+    print("The average recall score is: " + str(avgRC))
     print("")
     
-print("Program finished. Total elapsed time is: " + str(endtime - starttime))
-
-avgPQ = np.average(PQBS)
-avgPC = np.average(PCBS)
-avgF1asterisks = np.average(F1asteriskBS)
-avgF1 = np.average(F1BS)
-avgPR = np.average(PR)
-avgRC = np.average(RC)
-    
-print("")
-print("The average pair quality is: " + str(avgPQ*100) + "%.")
-print("The average pair completeness is: " + str(avgPC*100) + "%.")
-print("The average F1* score is: " + str(avgF1asterisks))
-print("The average F1 score is: " + str(avgF1))
-print("The average precision score is: " + str(avgPR))
-print("The average recall score is: " + str(avgRC))
-print("")
-
+    results[band].append(avgPQ)
+    results[band].append(avgPC)
+    results[band].append(avgF1asterisks)
+    results[band].append(avgF1)
+    results[band].append(avgPR)
+    results[band].append(avgRC)
